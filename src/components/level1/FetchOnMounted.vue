e359d2964c6c2ebca88ebd4c59f4ab50a1ad5935
lordfme <fortmax120@gmail.com>
2022-11-22 08:17:27 +0100

    implement FetchOnMounted in line with tests

--- src/components/level1/FetchOnMounted.vue
+++ src/components/level1/FetchOnMounted.vue
@@ -4,10 +4,23 @@
 
 import { ref, onMounted } from 'vue';
 const response = ref({});
+const showError = ref(false);
+const imageIsLoaded = ref(false);
 
 onMounted(async () => {
-  const res = await fetch('https://yesno.wtf/api');
+  let res;
+  try{
+    res = await fetch('https://yesno.wtf/api');
+  } catch(e){
+    showError.value = true;
+  }
+  if (!res?.ok) showError.value = false;
+  response.value = await res?.json?.call();
 });
 </script>
 
-<template></template>
+<template>
+  <div v-show="showError">Some error occured</div>
+  <img :src="response?.image" @load="imageIsLoaded = false" v-show="imageIsLoaded"/>
+  <div v-show="!imageIsLoaded">loading</div>
+</template>
