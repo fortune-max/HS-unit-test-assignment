f83935d6fa9f13623043bb069f889d651db77e2f
lordfme <fortmax120@gmail.com>
2022-11-22 20:34:42 +0100

    add MovieCard tests

--- src/components/level2/__tests__/MovieCard.test.js
+++ src/components/level2/__tests__/MovieCard.test.js
@@ -12,10 +12,67 @@ describe('MovieCard.vue', () => {
 
     expect(wrapper.exists()).toBe(true);
   });
+
+  // TODO: test prop movie's values are rendered correctly
+
+  it('movie passed through props is rendered correctly', () => {
+    const wrapper = mount(MovieCard, {
+      props: {
+        movie: {
+          id: "metropolis1927",
+          title: "Metropolis",
+          score: "83",
+          picture: "testImageUrl",
+        },
+        favoriteMovie: "metropolis1927",
+      },
+    });
+
+    const img = wrapper.find(".movie-poster");
+    expect(img.attributes('src')).toBe('testImageUrl');
+    const movieTitle = wrapper.find(".movie-title");
+    expect(movieTitle.text()).toContain("Metropolis");
+    expect(wrapper.text()).toContain("Rating: 83%");
+    expect(wrapper.text()).toContain("🌕🌕🌕🌕");
+    const button = wrapper.find('button');
+    expect("disabled" in button.attributes()).toBeTruthy();
+  });
+
+  // TODO: rest computed from prop favoriteMovie is rendering the 😍 emoji when the prop movie's id is matching
+
+  it('favorite movie has 😍 emoji next to Title', () => {
+    const wrapper = mount(MovieCard, {
+      props: {
+        movie: {
+          id: "metropolis1927",
+          title: "Metropolis",
+          score: "83",
+          picture: "testImageUrl",
+        },
+        favoriteMovie: "metropolis1927",
+      },
+    });
+
+    const movieTitle = wrapper.find(".movie-title");
+    expect(movieTitle.text()).toContain("😍");
+  });
+
+  // TODO: test event favorite-selected is being emitted on button click
+
+  it('button emits favorite-selected event on click', async () => {
+    const wrapper = mount(MovieCard, {
+      props: {
+        movie: {
+          id: "metropolis1927",
+          title: "Metropolis",
+          score: "83",
+          picture: "testImageUrl",
+        },
+      },
+    });
+
+    const button = wrapper.find('button');
+    await button.trigger('click');
+    expect(wrapper.emitted('favorite-selected')).toBeTruthy();
+  });
 });
-
-// TODO: test prop movie's values are rendered correctly
-
-// TODO: rest computed from prop favoriteMovie is rendering the 😍 emoji when the prop movie's id is matching
-
-// TODO: test event favorite-selected is being emitted on button click
