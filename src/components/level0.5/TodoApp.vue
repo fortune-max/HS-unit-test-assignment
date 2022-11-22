06082fd2cf2db1312d7577462c479b6e258ddab4
lordfme <fortmax120@gmail.com>
2022-11-22 08:16:20 +0100

    implement TodoApp in line with tests

--- src/components/level0.5/TodoApp.vue
+++ src/components/level0.5/TodoApp.vue
@@ -8,7 +8,31 @@ const todos = ref([
     completed: false,
   },
 ]);
+
+const newTodo = ref("");
+
+function addTodo(){
+  todos.value.push(
+    {
+      id: todos.value.length + 1,
+      text: newTodo.value,
+      completed: false,
+    }
+  );
+}
+
 </script>
 <template>
-  <div></div>
+  <div v-for="todo in todos" data-test="todo" :class="{completed: todo.completed}">
+    <input type="checkbox" data-test="todo-checkbox" v-model="todo.completed"/>
+    {{todo.text}}
+  </div>
+  <input data-test="new-todo-input" v-model="newTodo"/>
+  <button data-test="new-todo-btn" @click="addTodo" >Add New Todo</button>
 </template>
+
+<style scoped>
+.completed {
+  text-decoration: line-through;
+}
+</style>
