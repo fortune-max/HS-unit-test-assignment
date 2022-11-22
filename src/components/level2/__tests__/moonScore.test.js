3263408a04548397fb42761f7325eb1a22f0c082
lordfme <fortmax120@gmail.com>
2022-11-22 18:41:10 +0100

    add moonScore tests

--- src/components/level2/__tests__/moonScore.test.js
+++ src/components/level2/__tests__/moonScore.test.js
@@ -18,4 +18,52 @@ describe('getMoonScore', () => {
     // {n} matches the previous regex token exactly n times
     expect(score).toMatch(/^(🌕){5}$/);
   });
+
+  // handle multiples of 20 (0%, 20%, ..., 100%)
+
+  it('should render multiples of twenty', () => {
+    for (let moonCount=0; moonCount<=5; moonCount++){
+      const score = getMoonScore(moonCount * 20);
+      expect(score).toBe('🌕'.repeat(moonCount));
+    }
+  });
+
+  // handle non-multiples of 20 (remainder present) by testing step boundaries
+
+  it('should render no halves, lower bound', () => {
+    const int_part = Math.floor(Math.random() * 4.99);
+    const score = getMoonScore((int_part + 0.01 ) * 20);
+    expect(score).toBe('🌕'.repeat(int_part));
+  });
+
+  it('should render no halves, upper bound', () => {
+    const int_part = Math.floor(Math.random() * 4.99);
+    const score = getMoonScore((int_part + 0.39) * 20);
+    expect(score).toBe('🌕'.repeat(int_part));
+  });
+
+  it('should render half a moon, lower bound', () => {
+    const int_part = Math.floor(Math.random() * 4.99);
+    const score = getMoonScore((int_part + 0.400001) * 20);  // handle floating point errs w/ +ve epsilon: 1e-6
+    expect(score).toBe('🌕'.repeat(int_part) + '🌗');
+  });
+
+  it ('should render half a moon, upper bound', () => {
+    const int_part = Math.floor(Math.random() * 4.99);
+    const score = getMoonScore((int_part + 0.5) * 20);
+    expect(score).toBe('🌕'.repeat(int_part) + '🌗');
+  });
+
+  it('should render full moon, lower bound', () => {
+    const int_part = Math.floor(Math.random() * 4.99);
+    const score = getMoonScore((int_part + 0.51) * 20);
+    expect(score).toBe('🌕'.repeat(int_part + 1));
+  });
+
+  it('should render full moon, upper bound', () => {
+    const int_part = Math.floor(Math.random() * 4.99);
+    const score = getMoonScore((int_part + 0.99) * 20);
+    expect(score).toBe('🌕'.repeat(int_part + 1));
+  });
+
 });
