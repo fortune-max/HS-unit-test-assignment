f8fc00748bcff45587f8b164b18001bf462a8628
lordfme <fortmax120@gmail.com>
2022-11-23 02:20:16 +0100

    cleaner external library handling

--- src/components/level2/__tests__/Rating.test.js
+++ src/components/level2/__tests__/Rating.test.js
@@ -1,7 +1,7 @@
 import { expect, it, describe, vi } from 'vitest';
 import { mount } from '@vue/test-utils';
 import Ratings from '../Rating.vue';
-const moonScore = import('../utils/moonScore');
+import * as moonScore from '../utils/moonScore';
 
 describe('Ratings.vue', () => {
   it('renders correctly', () => {
@@ -11,7 +11,7 @@ describe('Ratings.vue', () => {
 
   // TODO: how to test for prop score here?
   it('Ensure Rating I/O is same as previously tested getMoonScore I/O', async() => {
-    const getMoonScore = vi.spyOn(await moonScore, 'getMoonScore').mockReturnValue("testVal");
+    const getMoonScore = vi.spyOn(moonScore, 'getMoonScore').mockReturnValue("testVal");
 
     const wrapper = mount(Ratings, {
       props: {
