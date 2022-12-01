a12a57c3defae31a716df556c10db36bd7fd1318
lordfme <fortmax120@gmail.com>
2022-12-01 01:45:35 +0100

    add most level3 tests

--- src/components/level3/FetchSuspense.test.js
+++ src/components/level3/FetchSuspense.test.js
@@ -1,14 +1,56 @@
-import { describe, it } from 'vitest';
+import { flushPromises, mount } from '@vue/test-utils';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import FetchSuspense from './FetchSuspense.vue';
+import ParentTestStub from './ParentTestStub.vue';
 // TODO: complete the test suite for this component!
 
 describe('FetchSuspense.vue', () => {
-  it('renders correctly', () => {});
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
 
-  it('calls fetch API', () => {});
+  it('renders correctly', () => {
+    const wrapper = mount(FetchSuspense);
+    expect(wrapper.exists()).toBe(true);
+  });
 
-  it('throws error on fetch not ok', () => {});
+  it('calls fetch API', () => {
+    const fetchSpy = vi.fn();
+    vi.stubGlobal('fetch', fetchSpy);
+    const wrapper = mount(FetchSuspense);
+    expect(fetchSpy).toBeCalledTimes(1);
+    expect(fetchSpy).toBeCalledWith('https://yesno.wtf/api');
+  });
 
-  it('throws error on fetch error', () => {});
+  it('throws error on fetch not ok', async () => {
+    const fetchSpy = vi.fn();
+    vi.stubGlobal('fetch', fetchSpy);
+    fetchSpy.mockImplementation(()=>{
+      return Promise.resolve({ok: false});
+    });
+
+    const wrapper = mount(ParentTestStub);
+    expect(wrapper.exists()).toBe(true);
+    expect(fetchSpy).toBeCalledTimes(1);
+    expect(fetchSpy).toBeCalledWith('https://yesno.wtf/api');
+    await flushPromises();
+    expect(wrapper.emitted('error-with-child')).toBeTruthy();
+  });
+
+  it('throws error on fetch error', async () => {
+    const fetchSpy = vi.fn();
+    vi.stubGlobal('fetch', fetchSpy);
+    fetchSpy.mockImplementation(()=>{
+      return Promise.reject();
+    });
+
+    const wrapper = mount(ParentTestStub);
+    expect(wrapper.exists()).toBe(true);
+    expect(fetchSpy).toBeCalledTimes(1);
+    expect(fetchSpy).toBeCalledWith('https://yesno.wtf/api');
+    await flushPromises();
+    expect(wrapper.emitted('error-with-child')).toBeTruthy();
+  });
 
   it('shows image once fetch is completed', () => {});
 });
---
+++ src/components/level3/ParentTestStub.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import { onErrorCaptured } from 'vue';
+    import FetchSuspense from './FetchSuspense.vue';
+    const emit = defineEmits(["error-with-child"]);
+
+    onErrorCaptured((err) => {
+        emit('error-with-child');
+        return false;
+    });
+</script>
+
+<template>
+    <Suspense>
+        <FetchSuspense />
+        <template #fallback>
+            Suspense fallback...
+        </template>
+    </Suspense>
+</template>
